72dd3ecff3d1a526ab1126af2de753d2f9f71dc5
Philippe Ngom <philngom@gmail.com>
2022-02-03 15:26:32 -0800

    added loading to all components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { fetchAllCars, fetchAllCountries, fetchAllDogs, fetchPeople } from './se
 import CountriesList from './CountriesList/CountriesList';
 import DogsList from './DogsList/DogsList';
 import PeopleList from './PeopleList/PeopleList';
+import loading from './loading.gif';
 
 function App() {
 
@@ -12,28 +13,37 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [dogs, setDogs] = useState([]);
   const [people, setPeople] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function getCars() {
+    setIsLoading(true);
     const data = await fetchAllCars();
 
+    setIsLoading(false);
     setCars(data);
   }
 
   async function getCountries() {
+    setIsLoading(true);
     const data = await fetchAllCountries();
 
+    setIsLoading(false);
     setCountries(data);
   }
 
   async function getDogs() {
+    setIsLoading(true);
     const data = await fetchAllDogs();
 
+    setIsLoading(false);
     setDogs(data);
   }
 
   async function getPeople() {
+    setIsLoading(true);
     const data = await fetchPeople();
 
+    setIsLoading(false);
     setPeople(data);
   }
 
@@ -46,10 +56,26 @@ function App() {
 
   return (
     <div className="App">
-      <CarsList cars={cars}/>
-      <CountriesList countries={countries}/>
-      <DogsList dogs={dogs}/>
-      <PeopleList people={people}/>
+      <h3>Cars</h3>
+      {
+        isLoading ? <img src={loading}/>
+          : <CarsList cars={cars}/>
+      }
+      <h3>Countries</h3>
+      {
+        isLoading ? <img src={loading}/>
+          : <CountriesList countries={countries}/>
+      }
+      <h3>Dogs</h3>
+      {
+        isLoading ? <img src={loading}/>
+          : <DogsList dogs={dogs}/>
+      }
+      <h3>People</h3>
+      {
+        isLoading ? <img src={loading}/>
+          : <PeopleList people={people}/>
+      }
     </div>
   );
 }
